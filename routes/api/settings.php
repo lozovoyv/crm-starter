850041b708afda3d115ee0255d35ba43fdde7677
lv <lozovoyv@gmail.com>
2022-10-15 19:08:45 +0300

    update roles

--- routes/api/settings.php
+++ routes/api/settings.php
@@ -2,13 +2,18 @@
 
 use App\Http\Controllers\API\Settings\Roles\PermissionsListController;
 use App\Http\Controllers\API\Settings\Roles\RolesController;
+use App\Http\Controllers\API\Settings\Roles\RolesEditController;
 use App\Http\Controllers\API\Settings\Roles\RolesHistoryController;
 use App\Http\Controllers\API\Settings\Roles\RolesListController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/api/settings/roles', [RolesListController::class, 'list']);
+Route::post('/api/settings/roles/get', [RolesEditController::class, 'get']);
+Route::post('/api/settings/roles/update', [RolesEditController::class, 'update']);
 Route::post('/api/settings/roles/activate', [RolesController::class, 'activate']);
 Route::post('/api/settings/roles/deactivate', [RolesController::class, 'deactivate']);
 Route::post('/api/settings/roles/remove', [RolesController::class, 'remove']);
 Route::post('/api/settings/roles/history', [RolesHistoryController::class, 'list']);
+Route::post('/api/settings/roles/history/comments', [RolesHistoryController::class, 'comments']);
+Route::post('/api/settings/roles/history/changes', [RolesHistoryController::class, 'changes']);
 Route::post('/api/settings/roles/permissions', [PermissionsListController::class, 'list']);
