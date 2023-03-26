718fd18875d3e40508f6ba052e3ad755309c6231
lv <lozovoyv@gmail.com>
2023-03-26 12:13:47 +0300

    update list response

---
+++ app/Http/Responses/ApiResponseList.php
@@ -0,0 +1,172 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Responses;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
+
+class ApiResponseList extends ApiResponse
+{
+    protected int $statusCode = self::CODE_OK;
+
+    protected array|Arrayable|Collection|LengthAwarePaginator $list = [];
+    protected ?array $filters;
+    protected ?string $title;
+    protected ?array $titles;
+    protected ?string $search;
+    protected ?string $order;
+    protected ?string $orderBy;
+    protected ?array $ordering;
+
+    /**
+     * Get response.
+     *
+     * @param Request $request
+     *
+     * @return  JsonResponse
+     *
+     * @throws InvalidArgumentException
+     */
+    public function toResponse($request): JsonResponse
+    {
+        if ($this->list instanceof LengthAwarePaginator) {
+            $list = array_values($this->list->items());
+            $pagination = [
+                'current_page' => $this->list->currentPage(),
+                'last_page' => $this->list->lastPage(),
+                'from' => $this->list->firstItem(),
+                'to' => $this->list->lastItem(),
+                'total' => $this->list->total(),
+                'per_page' => $this->list->perPage(),
+            ];
+        } else if ($this->list instanceof Arrayable) {
+            $list = array_values($this->list->toArray());
+            $pagination = null;
+        } else if (is_array($this->list)) {
+            $list = $this->list;
+            $pagination = null;
+        }
+
+        return new JsonResponse(
+            static::combine([
+                'list' => $list,
+            ], [
+                'pagination' => $pagination ?? null,
+                'title' => $this->title ?? null,
+                'titles' => $this->titles ?? null,
+                'filters' => $this->filters ?? null,
+                'search' => $this->search ?? null,
+                'order_by' => $this->orderBy ?? null,
+                'order' => $this->order ?? null,
+                'orderable' => $this->ordering ?? null,
+                'message' => $this->message ?? null,
+                'payload' => $this->payload ?? null,
+            ]), $this->statusCode, $this->getHeaders()
+        );
+    }
+
+    /**
+     * List items.
+     *
+     * @param array|Arrayable|Collection|LengthAwarePaginator $list
+     *
+     * @return  $this
+     */
+    public function items(array|Arrayable|Collection|LengthAwarePaginator $list = []): ApiResponseList
+    {
+        $this->list = $list;
+
+        return $this;
+    }
+
+    /**
+     * List title.
+     *
+     * @param string|null $title
+     *
+     * @return  $this
+     */
+    public function title(?string $title): ApiResponseList
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * List column titles.
+     *
+     * @param array|null $titles
+     *
+     * @return  $this
+     */
+    public function titles(?array $titles): ApiResponseList
+    {
+        $this->titles = $titles;
+
+        return $this;
+    }
+
+    /**
+     * List current filters.
+     *
+     * @param array|null $filters
+     *
+     * @return  $this
+     */
+    public function filters(?array $filters): ApiResponseList
+    {
+        $this->filters = $filters;
+
+        return $this;
+    }
+
+    /**
+     * List applied search.
+     *
+     * @param string|null $search
+     *
+     * @return  $this
+     */
+    public function search(?string $search): ApiResponseList
+    {
+        $this->search = $search;
+
+        return $this;
+    }
+
+    /**
+     * List applied order.
+     *
+     * @param string|null $orderBy
+     * @param string|null $order
+     *
+     * @return  $this
+     */
+    public function order(?string $orderBy, ?string $order): ApiResponseList
+    {
+        $this->order = $order;
+        $this->orderBy = $orderBy;
+
+        return $this;
+    }
+
+    /**
+     * List ordering options.
+     *
+     * @param array|null $ordering
+     *
+     * @return  $this
+     */
+    public function orderable(?array $ordering): ApiResponseList
+    {
+        $this->ordering = $ordering;
+
+        return $this;
+    }
+}
---
+++ tests/Http/Responses/ApiResponseListTest.php
@@ -0,0 +1,140 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Http\Responses;
+
+use App\Http\Responses\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+class ApiResponseListTest extends TestCase
+{
+    public function test_http_response_list(): void
+    {
+        $request = new Request();
+        $response = ApiResponse::list();
+        $result = $response->toResponse($request);
+
+        $this->assertEquals(200, $result->status());
+
+        $this->assertJson($result->content());
+
+        $this->assertJsonStringEqualsJsonString(
+            $result->content(), json_encode([
+                'list' => [],
+            ], JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function test_http_response_list_array(): void
+    {
+        $request = new Request();
+        $response = ApiResponse::list()->items([1, 2, 3]);
+        $result = $response->toResponse($request);
+
+        $this->assertEquals(200, $result->status());
+
+        $this->assertJson($result->content());
+
+        $this->assertJsonStringEqualsJsonString(
+            $result->content(), json_encode([
+                'list' => [1, 2, 3],
+            ], JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function test_http_response_list_collection(): void
+    {
+        $request = new Request();
+        $response = ApiResponse::list()->items(collect([1, 2, 3]));
+        $result = $response->toResponse($request);
+
+        $this->assertEquals(200, $result->status());
+
+        $this->assertJson($result->content());
+
+        $this->assertJsonStringEqualsJsonString(
+            $result->content(), json_encode([
+                'list' => [1, 2, 3],
+            ], JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function test_http_response_list_pagination(): void
+    {
+        $request = new Request();
+        $paginator = new LengthAwarePaginator(collect([1, 2, 3]), 3, 3, 1);
+        $response = ApiResponse::list()->items($paginator);
+        $result = $response->toResponse($request);
+
+        $this->assertEquals(200, $result->status());
+
+        $this->assertJson($result->content());
+
+        $this->assertJsonStringEqualsJsonString(
+            $result->content(), json_encode([
+                'list' => [1, 2, 3],
+                'pagination' => [
+                    'current_page' => 1,
+                    'last_page' => 1,
+                    'from' => 1,
+                    'to' => 3,
+                    'total' => 3,
+                    'per_page' => 3,
+                ],
+            ], JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function test_http_response_list_options(): void
+    {
+        $request = new Request();
+        $paginator = new LengthAwarePaginator(collect([1, 2, 3]), 3, 3, 1);
+        $response = ApiResponse::list()
+            ->items($paginator)
+            ->title('test title')
+            ->titles(['column1' => 'column1title', 'column2' => 'column2title'])
+            ->filters(['status' => 'test'])
+            ->search('search')
+            ->order('field', 'asc')
+            ->orderable(['field', 'field2'])
+            ->message('message')
+            ->payload(['payload']);
+        $result = $response->toResponse($request);
+
+        $this->assertEquals(200, $result->status());
+
+        $this->assertJson($result->content());
+
+        $this->assertJsonStringEqualsJsonString(
+            $result->content(), json_encode([
+                'list' => [1, 2, 3],
+                'pagination' => [
+                    'current_page' => 1,
+                    'last_page' => 1,
+                    'from' => 1,
+                    'to' => 3,
+                    'total' => 3,
+                    'per_page' => 3,
+                ],
+                'title' => 'test title',
+                'titles' => ['column1' => 'column1title', 'column2' => 'column2title'],
+                'filters' => ['status' => 'test'],
+                'search' => 'search',
+                'order_by' => 'field',
+                'order' => 'asc',
+                'orderable' => ['field', 'field2'],
+                'message' => 'message',
+                'payload' => ['payload'],
+            ], JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function test_http_response_list_invalid(): void
+    {
+        $this->expectException(TypeError::class);
+        ApiResponse::list()->items('error');
+    }
+}
