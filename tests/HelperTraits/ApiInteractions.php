ccf3e1ba0bc4569261933d55e114e8b967b1a874
lv <lozovoyv@gmail.com>
2023-04-13 07:47:24 +0300

    fix sanctum testing statefull

--- tests/HelperTraits/ApiInteractions.php
+++ tests/HelperTraits/ApiInteractions.php
@@ -10,17 +10,27 @@ use Laravel\Sanctum\Sanctum;
 
 trait ApiInteractions
 {
-    public function getApi(string $url, ?User $user = null, array $session = [], array $headers = []): TestResponse
+    private function initialize(?User $user, array $session): void
     {
         if ($user) {
             $this->actingAs($user)->withSession($session);
             Sanctum::actingAs($user);
         }
         Current::unset();
+    }
 
+    private function makeHeaders(array $headers): array
+    {
         $headers['Accept'] = 'application/json';
         $headers['origin'] = '127.0.0.1';
 
-        return $this->get($url, $headers);
+        return $headers;
+    }
+
+    public function getApi(string $url, ?User $user = null, array $session = [], array $headers = []): TestResponse
+    {
+        $this->initialize($user, $session);
+
+        return $this->get($url, $this->makeHeaders($headers));
     }
 }
