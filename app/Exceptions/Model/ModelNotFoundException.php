048f717b8e06570cb0d57f1a90ec8aba23b73519
lv <lozovoyv@gmail.com>
2023-08-20 13:11:12 +0300

    add license

--- app/Exceptions/Model/ModelNotFoundException.php
+++ app/Exceptions/Model/ModelNotFoundException.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of Opxx Starter project
+ *
+ * (c) Viacheslav Lozovoy <vialoz@yandex.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace App\Exceptions\Model;
