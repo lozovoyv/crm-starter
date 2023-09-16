6ead3f567a68f7ec91d28a7c090743f15e8ced09
lv <lozovoyv@gmail.com>
2023-09-16 12:25:53 +0300

    updates

--- front/App/Pages/System/HistoryPage.vue
+++ front/App/Pages/System/HistoryPage.vue
@@ -1,10 +1,16 @@
 <template>
-    <LayoutPage title="Журнал операций" :breadcrumbs="[{name: 'Журнал операций'}]">
+    <LayoutPage
+        title="Журнал операций"
+        :breadcrumbs="[{name: 'Журнал операций'}]"
+        :reload="true"
+        @reload="callReload"
+    >
         <HistoryList
             url="/api/system/history"
             method="get"
             remember-prefix="system_history"
             empty-message="Нет истории операций"
+            ref="history"
         />
     </LayoutPage>
 </template>
@@ -12,4 +18,13 @@
 <script setup lang="ts">
 import LayoutPage from "@/Components/Layout/LayoutPage.vue";
 import HistoryList from "@/Components/History/HistoryList.vue";
+import {ref} from "vue";
+
+const history = ref<InstanceType<typeof HistoryList> | undefined>(undefined);
+
+function callReload(): void {
+    if (history.value !== undefined) {
+        history.value.reload();
+    }
+}
 </script>
